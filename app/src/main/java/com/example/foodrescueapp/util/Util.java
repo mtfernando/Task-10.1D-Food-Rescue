64afe48df1d8bdd6e3e76dde46b26be80d8f953b
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-11 15:54:01 +1000

    DB Util Created

---
+++ app/src/main/java/com/example/foodrescueapp/util/Util.java
@@ -0,0 +1,22 @@
+package com.example.foodrescueapp.util;
+
+public class Util {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "user_db";
+    public static final String USER_TABLE_NAME = "users";
+    public static final String FOOD_TABLE_NAME = "food";
+
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String PHONE = "phone";
+    public static final String ADDRESS = "address";
+    public static final String NAME = "name";
+
+    public static final String FOOD_ID = "food_id";
+    public static final String FOOD_TITLE = "food_title";
+    public static final String FOOD_DESCRIPTION = "food_description";
+    public static final String FOOD_DATE = "food_date";
+    public static final String FOOD_PICKUP_TIME = "food_pickup_time";
+    public static final String FOOD_QUANTITY = "food_quantity";
+    public static final String FOOD_LOCATION = "food_location";
+}
