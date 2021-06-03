c3943ef0648675dfe917c6086afe9a1077c56a57
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-06-03 16:10:29 +1000

    CalendarActivity bug fix - showing improper month

--- app/src/main/java/com/example/foodrescueapp/CalendarActivity.java
+++ app/src/main/java/com/example/foodrescueapp/CalendarActivity.java
@@ -49,7 +49,7 @@ public class CalendarActivity extends AppCompatActivity {
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(CalendarActivity.this, dayOfMonth + "/" + month+1 + "/" + year, Toast.LENGTH_SHORT).show();
+                Toast.makeText(CalendarActivity.this, dayOfMonth + "/" + (month+1) + "/" + year, Toast.LENGTH_SHORT).show();
                 date = String.valueOf(dayOfMonth) + "/" + String.valueOf(month+1) + "/" + String.valueOf(year);
 
                 dateTextView.setText(date);
